41b67ffb38be943f1f96bdea10715a4326fe8b49
AVAniketh0905 <adimoolamaniketh@gmail.com>
2026-02-08 16:14:55 +0000

    added bufferline

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,13 +1,65 @@
 function ColorMyPencils(color)
     color = color or "catppuccin"
+
+    require("catppuccin").setup({
+        flavour = "mocha",
+        integrations = {
+            bufferline = true,
+            nvimtree = true,
+            treesitter = true,
+            telescope = true,
+        }
+    })
+
     vim.cmd.colorscheme(color)
 
-    -- Setup
-    --require("catppuccin").setup({ flavour = "mocha" }) -- or "macchiato"
-    --vim.cmd.colorscheme "catppuccin"
+    require("bufferline").setup({
+        options = {
+            mode = "buffers",
+            separator_style = "thin",
+            always_show_bufferline = true,
+            show_buffer_close_icons = false,
+            show_close_icon = false,
+            color_icons = true,
+
+            indicator = {
+                style = 'none',
+            },
+
+            -- Formatting: Icons + Name + Extension (Default behavior)
+            show_buffer_icons = true,
+            show_tab_indicators = false,
+            persist_buffer_sort = true,
+
+            -- Disable italics globally for the tabline
+            italic = false,
+        },
+        highlights = {
+            buffer_selected = {
+                italic = false,
+                bold = true,
+            },
+            modified_selected = {
+                italic = false,
+            },
+        }
+    })
+
     vim.api.nvim_set_hl(0, "@underline", { link = "Underlined" })
-    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
+
+-- Tab navigation (Shift + h/l)
+vim.keymap.set("n", "H", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Tab" })
+vim.keymap.set("n", "L", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Tab" })
+vim.keymap.set("n", "gt", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Tab" })
+vim.keymap.set("n", "gT", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Tab" })
+
+-- Reorder tabs (Control + h/l)
+vim.keymap.set("n", "<C-h>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move Tab Left" })
+vim.keymap.set("n", "<C-l>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move Tab Right" })
+
+-- Close tab
+vim.keymap.set("n", "<leader>x", "<cmd>bdelete<cr>", { desc = "Close Tab" })
+vim.keymap.set("n", "<cmd>q", "<cmd>bdelete<cr>", { desc = "Close Tab" })
--- init.lua
+++ init.lua
@@ -1,2 +1,2 @@
 require("avaniketh")
-
+-- hi
